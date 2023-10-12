fe119c06dc83a33c43bf540521994e7e5e322277
Daniel Zych <danjzych@gmail.com>
2023-10-12 17:05:42 -0500

    added default photo for users

--- src/FriendCard.tsx
+++ src/FriendCard.tsx
@@ -3,11 +3,11 @@
 function FriendCard( {user} ){
     const {username, hobbies, interests, image_urls} = user;
 
-    const profilePic = image_urls[0];
+    const profilePic = image_urls[0] || './default-profile-pic.jpg'
 
     return (
         <div className="FriendCard">
-            <img src={profilePic} />
+            <img src={profilePic} width='200px'/>
             <h2>{username}</h2>
             <p>Hobbies are: {hobbies}</p>
             <p>Interests include: {interests}</p>
