6a4e6287eab3cd8694e24f63be38be333a63fe89
Michael <michaellngriffin@gmail.com>
2023-10-13 15:31:04 -0400

    fixed small redirect

--- src/ProfileImageForm.tsx
+++ src/ProfileImageForm.tsx
@@ -1,22 +1,25 @@
 import {useState} from 'react'
+import {useNavigate} from 'react-router-dom';
 interface ProfileImageFormProps {
     handleSubmit: ({ }) => void;
 }
 
 function ProfileImageform({handleSubmit}: ProfileImageFormProps) {
+    const navigate = useNavigate();
     const [file, setFile] = useState()
 
     function onFileChange(evt) {
         setFile(evt.target.files[0])
     }
 
+
     function handleFileSubmit(evt){
         evt.preventDefault();
         //JS way is to grab whole form, then convert that into form data:
         const formData = new FormData();
         formData.append('image', file)
-
         handleSubmit(formData);
+        navigate("/profile");
     }
 
     return (<form onSubmit={handleFileSubmit} className='ProfileImageForm'>
